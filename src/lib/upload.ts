d274520c9855090af3ebb4286f4a6e00a4cc8be3
@dmin <e.amaraa.3@live.com>
2024-02-15 13:08:53 +0800

    base64

--- src/lib/upload.ts
+++ src/lib/upload.ts
@@ -1,13 +1,19 @@
+import { rejects } from 'assert';
 import { writeFile } from 'fs/promises';
-import { join } from 'path';
+import { join, resolve } from 'path';
 import { v4 } from 'uuid';
 
-export default async (file: File) => {
-  if (!file.size) return null;
+export default async (file: File) =>
+  new Promise((resolve) => {
+    if (!file.size) return null;
 
-  const buffer = Buffer.from(await file.arrayBuffer());
-  const extention = file.name.split('.').pop();
-  const pathname = `/${Date.now()}-${v4()}.${extention}`;
-  await writeFile(join(process.cwd(), 'public' + pathname), buffer);
-  return pathname;
-};
+    // const buffer = Buffer.from(await file.arrayBuffer());
+    // const extention = file.name.split('.').pop();
+    // const pathname = `/${Date.now()}-${v4()}.${extention}`;
+    // await writeFile(join(process.cwd(), 'public' + pathname), buffer);
+    // return pathname;
+
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+  });
