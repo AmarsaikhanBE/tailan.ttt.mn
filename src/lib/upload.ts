c8efd72e78fe60bfa1d4fecbef92b58ce8d701f4
@dmin <e.amaraa.3@live.com>
2024-02-15 13:25:15 +0800

    back file upload

--- src/lib/upload.ts
+++ src/lib/upload.ts
@@ -5,11 +5,11 @@ import { v4 } from 'uuid';
 export default async (file: File) => {
   if (!file.size) return null;
 
-  // const buffer = Buffer.from(await file.arrayBuffer());
-  // const extention = file.name.split('.').pop();
-  // const pathname = `/${Date.now()}-${v4()}.${extention}`;
-  // await writeFile(join(process.cwd(), 'public' + pathname), buffer);
-  // return pathname;
+  const buffer = Buffer.from(await file.arrayBuffer());
+  const extention = file.name.split('.').pop();
+  const pathname = `/${Date.now()}-${v4()}.${extention}`;
+  await writeFile(join(process.cwd(), 'public' + pathname), buffer);
+  return pathname;
 
-  return URL.createObjectURL(file);
+  // return URL.createObjectURL(file);
 };
